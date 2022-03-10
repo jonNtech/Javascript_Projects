30c534b4fd22c840604ed6b1b7c0513bd9caaa7b
Jon Pena <31232174+jonNtech@users.noreply.github.com>
2022-03-09 20:19:50 -0700

    space repetition with JS

---
+++ stop-snacking/js/main.js
@@ -0,0 +1,10 @@
+//Create a function that grabs the number of snacks from the input and tells you to stop that many times
+document.querySelector('#help').addEventListener('click', stopSnacking)
+
+function stopSnacking() {
+    let timesSaid = Number(document.querySelector('input').value)
+    document.querySelector('#stops').innerText = ''
+    for (let i = 1; i < timesSaid + 1; i++) {
+        document.querySelector('#stops').innerText += ' Stop Snacking'
+    }
+}
